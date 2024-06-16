4d7662becce149b6d895009fda522261a12056db
chucky <bozicslxye1@gmail.com>
2024-06-16 14:50:35 -0500

    remove the default hibernate seq generation

--- Course Registration/src/main/java/com/chucky/school/domain/Course.java
+++ Course Registration/src/main/java/com/chucky/school/domain/Course.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -22,7 +23,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Course {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private long credits;
   private String courseName;
