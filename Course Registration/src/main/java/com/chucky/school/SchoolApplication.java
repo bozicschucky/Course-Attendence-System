e6005d90592806044bfe86aa94a0ee96dc8a16e6
chucky <bozicslxye1@gmail.com>
2024-06-16 14:50:59 -0500

    add a few configs

--- Course Registration/src/main/java/com/chucky/school/SchoolApplication.java
+++ Course Registration/src/main/java/com/chucky/school/SchoolApplication.java
@@ -3,9 +3,13 @@ package com.chucky.school;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.chucky.school.repository")
+@EntityScan(basePackages = "com.chucky.school.domain")
 public class SchoolApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
