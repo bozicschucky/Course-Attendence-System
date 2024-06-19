912545b083a7f632b5811facbe44961f8ce4d696
chucky <bozicslxye1@gmail.com>
2024-06-19 12:29:34 -0500

    resolve merge conflicts

--- Course Registration/src/main/java/com/chucky/school/repository/SessionRepository.java
+++ Course Registration/src/main/java/com/chucky/school/repository/SessionRepository.java
@@ -2,12 +2,12 @@ package com.chucky.school.repository;
 
 import com.chucky.school.domain.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.Optional;
 
+@Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
     Optional<Session> findBySessionDate(LocalDate sessionDate);
     Optional<Session> findById(Long id);
