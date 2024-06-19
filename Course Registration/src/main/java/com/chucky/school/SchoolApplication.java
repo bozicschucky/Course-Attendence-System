50ef6ae36b8e5ff37e16f5152a76b3a11f13a10b
kalkidanakalu <kalkidant3@gmail.com>
2024-06-19 10:50:07 -0500

    kafka configuration

--- Course Registration/src/main/java/com/chucky/school/SchoolApplication.java
+++ Course Registration/src/main/java/com/chucky/school/SchoolApplication.java
@@ -1,21 +1,30 @@
 package com.chucky.school;
 
+import com.chucky.school.domain.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 
 @SpringBootApplication
-
+@EnableKafka
 @EnableJpaRepositories(basePackages = "com.chucky.school.repository")
 @EntityScan(basePackages = "com.chucky.school.domain")
 public class SchoolApplication implements CommandLineRunner {
-
+	@Autowired
+	Sender sender;
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
+
 	@Override
-	public void run(String... args) throws Exception {}
+	public void run(String... args) throws Exception {
+		sender.send("topicA", "Hello World");
+		System.out.println("Message has been sent");
+	}
+
 }
